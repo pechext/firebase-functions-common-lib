0597eed1ce7c71eae737fcbfe791efca63b60409
pechext <pechext@gmail.com>
2025-06-23 16:59:15 +0300

    fix express types

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,12 +1,12 @@
-import * as express from 'express';
+import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 
-export const createExpressApp = (): express.Application => {
+export const createExpressApp = (): express.Express => {
   const corsMiddleware = cors({ origin: true });
   const cookieParserMiddleware = cookieParser();
 
-  const app = express.default();
+  const app = express();
 
   app.use(corsMiddleware);
   app.use(cookieParserMiddleware);
